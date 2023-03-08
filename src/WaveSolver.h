9b1d26477e13b9a3475dc9ab5f769cc2c896040d
Jdinis8 <joaodinis01@gmail.com>
2023-03-08 18:02:03 +0000

    minor changes

--- src/WaveSolver.h
+++ src/WaveSolver.h
@@ -9,18 +9,20 @@ class WaveSolver{
         WaveSolver();
         ~WaveSolver() = default;
 
-        //Time Methods
-        void TimeSimpleDiff1(double** data, int size_t, int size_x, double space_step, double time_step, std::vector<double> RHS); // we assume data[i][j] means ith step of time, jth step of space
+        //In the following methods, we assume data[i][j] means ith step of time, jth step of space
+        
+        // Time Methods
+        void TimeSimpleDiff1  (double** data, int size_t, int size_x, double space_step, double time_step, std::vector<double> RHS); 
         void TimeCenteredDiff2(double **data, int size_t, int size_x, double space_step, double time_step, std::vector<double> RHS);
 
         //Periodic Spatial Methods
-        std::vector<double> PFirstDerSpaceCenteredDiff2(double **data, int size_t, int size_x, double space_step);
-        std::vector<double> PFirstDerSpaceCenteredDiff4(double **data, int size_t, int size_x, double space_step);
+        std::vector<double> PFirstDerSpaceCenteredDiff2 (double **data, int size_t, int size_x, double space_step);
+        std::vector<double> PFirstDerSpaceCenteredDiff4 (double **data, int size_t, int size_x, double space_step);
         std::vector<double> PSecondDerSpaceCenteredDiff2(double **data, int size_t, int size_x, double space_step);
         
 
         //Non-Periodic Spatial Methods
-        std::vector<double> FirstDerSpaceCenteredDiff2(double **data, int size_t, int size_x, double space_step);
+        std::vector<double> FirstDerSpaceCenteredDiff2 (double **data, int size_t, int size_x, double space_step);
         std::vector<double> SecondDerSpaceCenteredDiff2(double **data, int size_t, int size_x, double space_step);
 
         std::vector<double> RK4(double **data, int size_t, int size_x, double space_step, double time_step);
